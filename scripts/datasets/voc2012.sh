7e934941fb152e48f6af4226650dc2e1964c3b59
Cenk Bircanoglu <cenk.bircanoglu@adevinta.com>
2020-01-12 13:09:49 +0100

    script added

--- scripts/datasets/voc2012.sh
+++ scripts/datasets/voc2012.sh
@@ -1,15 +1,15 @@
 python3 wsl_survey/datasets/voc/2012/download.py \
-    --dataset_dir=datasets/voc2012
+    --dataset_dir=/Volumes/2TB/datasets/voc2012
 
 python3 wsl_survey/datasets/voc/preprocess/labelled_dataset.py \
-    --dataset_dir=datasets/voc2012/data \
-    --output_dir=datasets/voc2012/labelled
+    --dataset_dir=/Volumes/2TB/datasets/voc2012/data \
+    --output_dir=/Volumes/2TB/datasets/voc2012/labelled
 
 python3 wsl_survey/datasets/voc/preprocess/annotated_dataset.py \
-    --dataset_dir=datasets/voc2012/data \
-    --labelled_dir=datasets/voc2012/labelled \
-    --output_dir=datasets/voc2012/annotated
+    --dataset_dir=/Volumes/2TB/datasets/voc2012/data \
+    --labelled_dir=/Volumes/2TB/datasets/voc2012/labelled \
+    --output_dir=/Volumes/2TB/datasets/voc2012/annotated
 
 python3 wsl_survey/datasets/class_labels.py \
-    --source_dir=datasets/voc2012/labelled \
-    --target_dir=datasets/voc2012/labelled
+    --source_dir=/Volumes/2TB/datasets/voc2012/labelled \
+    --target_dir=/Volumes/2TB/datasets/voc2012/labelled
--- wsl_survey/segmentation/irn/voc12/make_cls_labels.py
+++ wsl_survey/segmentation/irn/voc12/make_cls_labels.py
@@ -7,18 +7,32 @@ from wsl_survey.segmentation.irn.voc12 import dataloader
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_list", default='train_aug.txt', type=str)
-    parser.add_argument("--val_list", default='val.txt', type=str)
-    parser.add_argument("--out", default="cls_labels.npy", type=str)
-    parser.add_argument("--voc12_root", default="../../../Dataset/VOC2012",
+    parser.add_argument("--train_aug_list",
+                        default='/Users/cenk.bircanoglu/wsl/wsl_survey/data/voc12/train_aug.txt',
+                        type=str)
+    parser.add_argument("--train_list",
+                        default='/Users/cenk.bircanoglu/wsl/wsl_survey/data/voc12/train.txt',
+                        type=str)
+    parser.add_argument("--val_list",
+                        default='/Users/cenk.bircanoglu/wsl/wsl_survey/data/voc12/val.txt',
+                        type=str)
+    parser.add_argument("--test_list",
+                        default='/Users/cenk.bircanoglu/wsl/wsl_survey/data/voc12/test.txt',
+                        type=str)
+    parser.add_argument("--out",
+                        default="/Users/cenk.bircanoglu/wsl/wsl_survey/data/voc12/cls_labels_test.npy",
+                        type=str)
+    parser.add_argument("--voc12_root",
+                        default="/Users/cenk.bircanoglu/wsl/wsl_survey/datasets/voc2012/tmp/VOCdevkit/VOC2012",
                         type=str)
     args = parser.parse_args()
 
-    train_name_list = dataloader.load_img_name_list(args.train_list)
-    val_name_list = dataloader.load_img_name_list(args.val_list)
+    test_list = dataloader.load_img_name_list(args.test_list)
 
-    train_val_name_list = np.concatenate([train_name_list, val_name_list],
-                                         axis=0)
+    train_val_name_list = np.concatenate(
+        [ test_list], axis=0)
+    train_val_name_list = np.unique(train_val_name_list)
+
     label_list = dataloader.load_image_label_list_from_xml(train_val_name_list,
                                                            args.voc12_root)
 
